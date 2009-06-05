d0188fcb52eca87aae06041ab26b90ba740866ee
Frank <frank.trindade@gmail.com>
2009-06-05 17:26:12 +0100

    first version

---
+++ top_analyzer.rb
@@ -0,0 +1,9 @@
+top_file = File.new(ARGV[0])
+begin
+    while (line = top_file.readline)
+        line.chomp
+        timestamp = line.match('top - (\d\d:\d\d:\d\d)')[1] if line.match('top - (\d\d:\d\d:\d\d)')
+    end
+rescue EOFError
+    top_file.close
+end
---
+++ top_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Top
+# -b -> batch mode
+# -n -> number of samples
+# -d -> deay between samples
+top -b -n 360 -d 10

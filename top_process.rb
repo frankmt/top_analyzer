29a627277cbf5a4c4e8bce4dbe46f36fe3254ccc
Frank <frank.trindade@gmail.com>
2009-06-05 23:58:49 +0100

    more specs

--- top_process.rb
+++ top_process.rb
@@ -1,9 +1,16 @@
 class TopProcess
   
-  # @@processes = []
+  @@all_processes = []
   attr_reader :pid
   
   def self.find_or_create(pid)
+    @@all_processes.each do |process|
+      return process if process.pid == pid
+    end
+    
+    new_process = TopProcess.new(pid)
+    @@all_processes << new_process
+    new_process
   end
   
   def initialize(pid)
--- top_process_spec.rb
+++ top_process_spec.rb
@@ -2,6 +2,26 @@ require 'top_process'
 
 describe TopProcess do
   
+  describe "class methods" do
+
+    it "should create new process when it does not exist" do
+      process = TopProcess.new(1234)
+      TopProcess.should_receive(:new).with(1234).and_return(process)
+      test_process = TopProcess.find_or_create(1234)
+      test_process.should == process
+    end
+    
+    it "should find process when it does exist" do
+      old_process = TopProcess.new(4321)
+      old_process.add_info("12:03:41","85%")
+      
+      test_process = TopProcess.find_or_create(4321)
+      test_process.get_info("12:03:41").should == 0    
+    end
+    
+
+  end
+  
   it "should initialize pid" do
     process = TopProcess.new(1234)
     process.pid.should == 1234
